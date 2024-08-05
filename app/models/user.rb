e2d9801cc6e296ee8ed886378d5b9f26595d6e3f
mr-vikash <mrvikash700@gmail.com>
2024-08-05 19:38:29 +0530

    implementing secure password using has_secure_password and bcrypt gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false }
   validates :email, presence: true, length: { maximum: 100 },
              uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
