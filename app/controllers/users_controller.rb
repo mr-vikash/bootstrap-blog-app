c68ca2de6df6f5079a93315a451fefdc1b889fbb
mr-vikash <mrvikash700@gmail.com>
2024-08-08 13:13:09 +0530

    adding current user functionality and adjusting nav bar using current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:show, :edit, :update]
+
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
@@ -11,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "You have signed up successfully"
       redirect_to users_path
     else
@@ -19,16 +22,13 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "#{@user.username} your details updated successfully"
       redirect_to @user #user_path also can be used
@@ -41,5 +41,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
